7b99de265ac99b2c24af223595018cc9ac87d931
Moshe Bergman <bergmanm@gmail.com>
2015-08-29 18:40:29 -0700

    add production settings to the site

--- client/config/config.js
+++ client/config/config.js
@@ -3,4 +3,12 @@ var config = {
   httpServer: 'http://localhost:8080/webpack-dev-server/'
 }
 
+// production settings
+if (window.location.href.indexOf('plot-my-trip.com') > 0) {
+  config = {
+    apiServer: 'http://www.plot-my-trip.com',
+    httpServer: 'http://www.plot-my-trip.com'
+  }
+}
+
 export default config;
