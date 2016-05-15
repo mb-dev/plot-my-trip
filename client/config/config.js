91626c3561876b0a3d98e59768687069be155d69
Moshe Bergman <mb-dev@users.noreply.github.com>
2016-05-14 23:24:47 -0700

    change config to use SSL

--- client/config/config.js
+++ client/config/config.js
@@ -1,16 +1,16 @@
 var config = {
   apiServer: 'http://www.plot-my-trip.local.com:8000',
   httpServer: 'http://localhost:8080/webpack-dev-server/',
-  iconServer: 'https://s3-us-west-1.amazonaws.com/plot-my-trip/'
-}
+  iconServer: 'https://s3-us-west-1.amazonaws.com/plot-my-trip/',
+};
 
 // production settings
 if (window.location.href.indexOf('plot-my-trip.com') > 0) {
   config = {
-    apiServer: 'http://www.plot-my-trip.com',
-    httpServer: 'http://www.plot-my-trip.com',
-    iconServer: 'https://s3-us-west-1.amazonaws.com/plot-my-trip/'
-  }
+    apiServer: 'https://www.plot-my-trip.com',
+    httpServer: 'https://www.plot-my-trip.com',
+    iconServer: 'https://s3-us-west-1.amazonaws.com/plot-my-trip/',
+  };
 }
 
 export default config;
